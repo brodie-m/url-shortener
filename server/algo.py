56fa6db585edf751318150fc478d63ba8a1a3759
brodie-m <brodiedmcguire@gmail.com>
2021-11-11 09:42:54 +0000

    review

--- server/algo.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-import base36
-
-def shorten_url(string):
-    hashed = int(hashlib.sha256(string.encode('utf-8')).hexdigest(), 16)
-    return {'id' : base36.dumps(hashed)[0:6], 'url': string}
-
-
